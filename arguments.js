24277b4b653ecc58aded478965cf71fd75f3e248
AbuMdNayeemKhan <abumohammadnayemkhan@gmail.com>
2024-02-02 22:06:44 +0600

    learning about javascript argument and parameter

---
+++ arguments.js
@@ -0,0 +1,18 @@
+function doubleIt (number){
+    const doubled = number * 2;
+    console.log(number, doubled);
+}
+
+console.log('I will call the function');
+doubleIt(15);
+console.log('=====================');
+doubleIt(88);
+console.log('=====================');
+doubleIt(873);
+
+function deferent(x, y){
+    let def = x - y;
+    console.log(def);
+};
+
+deferent(10, 9);
---
+++ index.js
@@ -0,0 +1,9 @@
+let x = "Ambulance";
+console.log(x.length);
+let rev = '';
+for(let i = x.length-1; i >= 0; i--){
+    rev += x[i];
+}
+console.log(rev);
+
+
